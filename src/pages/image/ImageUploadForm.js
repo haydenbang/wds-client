fcc64abc90c0a5d8c2fad1feac208182659c5a1e
MiNGMii <sak9994@gmail.com>
2019-11-14 00:49:42 +0900

    [SPRINT2][minji] onClick button (url 지정 필요)

--- src/pages/image/ImageUploadForm.js
+++ src/pages/image/ImageUploadForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './Image.css';
 import '../users/User.css';
@@ -6,22 +7,36 @@ import '../users/User.css';
 class ImageUploadForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {file: null}  // state 초기화
+    this.state = {file: null};  // state 초기화
+
+    // This binding is necessary to make `this` work in the callback
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   onChange(e) {
-    this.setState({file: e.target.files[0]})
+    this.setState({file: e.target.files[0]});
   }
 
   onClick() {
-
+    const formData = new FormData();
+    const url = "";
+    formData.append('file', this.state.file);
+    return axios.post(url, formData)
+        .then(res => {
+            console.log(res.data);
+            alert("이미지 업로드 완료되었습니다.");
+        }).catch(err => {
+            console.log(err.data);
+            alert("이미지 업로드 실패하였습니다.")
+        })
   }
 
   render() {
     return (
         <div>
           <input type="file" id="uploadInput" className="upload_input" name="file" onChange={e => this.onChange(e)} />
-          <button type="button" id="uploadButton" className="btn-gradient red" onClick={this.onClick()}>Upload</button>
+          <button type="button" id="uploadButton" className="btn-gradient red" onClick={this.onClick}>Upload</button>
         </div>
     );
   }
