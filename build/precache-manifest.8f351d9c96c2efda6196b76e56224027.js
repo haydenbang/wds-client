c18cb881fff4eb15c0acde805ad00bbd5df23cd0
kwjang <jkw0408@gmail.com>
2019-11-15 23:22:56 +0900

    test

---
+++ build/precache-manifest.8f351d9c96c2efda6196b76e56224027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d001ff417e9125a62c50b0ca11560831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec272830551cb95ed30",
+    "url": "/static/css/main.00a65da6.chunk.css"
+  },
+  {
+    "revision": "567213b22d5738cc708c",
+    "url": "/static/js/2.bf986551.chunk.js"
+  },
+  {
+    "revision": "0ec272830551cb95ed30",
+    "url": "/static/js/main.d69985f9.chunk.js"
+  },
+  {
+    "revision": "cba6963fba867aa99ce6",
+    "url": "/static/js/runtime-main.9e706f57.js"
+  }
+]);
